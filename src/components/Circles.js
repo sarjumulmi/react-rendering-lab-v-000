b0a086b3731ba725c44f915cbb65a9c8827e67eb
sarjumulmi <mulmi.sarju@gmail.com>
2017-09-07 22:24:33 -0400

    Done.

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -5,7 +5,7 @@ class Circles extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
       circles: [
         { color: 'red' },
@@ -13,6 +13,10 @@ class Circles extends React.Component {
         { color: 'green' },
       ]
     };
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return nextState.circles.color!==this.state.circles.color
   }
 
   handleClick = () => {
@@ -26,8 +30,8 @@ class Circles extends React.Component {
   }
 
   render() {
-    const renderCircles = this.state.circles.map((circle, index) => 
-      <Circle color={circle.color} key={index} /> 
+    const renderCircles = this.state.circles.map((circle, index) =>
+      <Circle color={circle.color} key={index} />
     );
 
     return (
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,13 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps){
+    this.setState((prevState, props)=>({
+      increasing: this.props.rating < nextProps.rating,
+      decreasing: this.props.rating > nextProps.rating,
+    }));
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
